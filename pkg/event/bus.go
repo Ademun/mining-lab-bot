50811215c08ca7fb5cf476b43af3b3807279f51b
Ademun <78663343+Ademun@users.noreply.github.com>
2025-10-10 14:51:46 +0300

    Implemented event bus

---
+++ pkg/event/bus.go
@@ -0,0 +1,40 @@
+package event
+
+import (
+	"fmt"
+	"reflect"
+	"sync"
+)
+
+type Bus struct {
+	subscribers map[reflect.Type][]interface{}
+	mu          sync.RWMutex
+}
+
+func NewEventBus() *Bus {
+	return &Bus{
+		subscribers: make(map[reflect.Type][]interface{}),
+		mu:          sync.RWMutex{},
+	}
+}
+
+func Subscribe[T any](eb *Bus, handler func(T)) {
+	eb.mu.Lock()
+	defer eb.mu.Unlock()
+
+	var zero T
+	eventType := reflect.TypeOf(zero)
+	eb.subscribers[eventType] = append(eb.subscribers[eventType], handler)
+}
+
+func Publish[T any](eb *Bus, event T) {
+	eb.mu.Lock()
+	defer eb.mu.Unlock()
+
+	fmt.Println(event)
+	eventType := reflect.TypeOf(event)
+	for _, h := range eb.subscribers[eventType] {
+		handler := h.(func(T))
+		handler(event)
+	}
+}
---
+++ pkg/event/types.go
@@ -0,0 +1,7 @@
+package event
+
+import "github.com/Ademun/mining-lab-bot/pkg/model"
+
+type NewSlotEvent struct {
+	Slot model.Slot
+}
