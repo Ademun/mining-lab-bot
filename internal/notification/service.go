2a51e045ecd5688e3ba0984ef51b13fe9445e689
Ademun <78663343+Ademun@users.noreply.github.com>
2025-10-13 22:03:34 +0300

    Refactored notification service

--- internal/notification/service.go
+++ internal/notification/service.go
@@ -9,31 +9,32 @@ import (
 	"github.com/Ademun/mining-lab-bot/internal/subscription"
 	"github.com/Ademun/mining-lab-bot/pkg/cache"
 	"github.com/Ademun/mining-lab-bot/pkg/event"
+	"github.com/Ademun/mining-lab-bot/pkg/logger"
 	"github.com/Ademun/mining-lab-bot/pkg/model"
 )
 
 type NotificationService interface {
-	Start(ctx context.Context) error
+	Start() error
 }
 
 type notificationService struct {
-	eb         *event.Bus
+	eventBus   *event.Bus
 	subService subscription.SubscriptionService
 	cache      *cache.TTLCache[model.Slot]
 }
 
 func New(eb *event.Bus, subService subscription.SubscriptionService) NotificationService {
 	return &notificationService{
-		eb:         eb,
+		eventBus:   eb,
 		subService: subService,
 		cache:      cache.NewTTLCache[model.Slot](time.Minute*5, time.Minute*10),
 	}
 }
 
-func (s *notificationService) Start(ctx context.Context) error {
-	slog.Info("[Notification service] Starting...")
-	event.Subscribe(s.eb, s.handleNewSlot)
-	slog.Info("[Notification service] Started")
+func (s *notificationService) Start() error {
+	slog.Info("Starting", "service", logger.ServiceNotification)
+	event.Subscribe(s.eventBus, s.handleNewSlot)
+	slog.Info("Started", "service", logger.ServiceNotification)
 	return nil
 }
 
@@ -42,12 +43,12 @@ func (s *notificationService) handleNewSlot(ctx context.Context, slot model.Slot
 	if !exists {
 		subs, err := s.subService.FindSubscriptionsBySlotInfo(ctx, slot)
 		if err != nil {
-			slog.Error("[Notification service] Failed to handle new slot", err)
+			slog.Error("Failed to find subscriptions for slot", "data", slot, "error", err, "service", logger.ServiceNotification)
 		}
 
 		for _, sub := range subs {
 			notif := model.Notification{UserID: sub.UserID, Slot: slot}
-			event.Publish(s.eb, ctx, &notif)
+			event.Publish(s.eventBus, ctx, &notif)
 		}
 	}
 
