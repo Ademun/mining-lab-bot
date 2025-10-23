c4fa964629f98d694f5441186d5e183d19852c0d
Ademun <78663343+Ademun@users.noreply.github.com>
2025-10-23 13:06:45 +0300

    Refined teacher service

--- internal/teacher/service.go
+++ internal/teacher/service.go
@@ -6,8 +6,10 @@ import (
 	"log/slog"
 	"time"
 
+	"github.com/Ademun/mining-lab-bot/pkg/config"
 	"github.com/Ademun/mining-lab-bot/pkg/logger"
 	"github.com/Ademun/mining-lab-bot/pkg/model"
+	"github.com/robfig/cron/v3"
 )
 
 type Service interface {
@@ -15,14 +17,44 @@ type Service interface {
 }
 
 type teacherService struct {
-	teacherRepo Repo
-	weekNumber  int
+	teacherRepo   Repo
+	options       config.TeacherConfig
+	weekNumber    int
+	cronScheduler *cron.Cron
 }
 
-func New(repo Repo) Service {
+func New(repo Repo, opts *config.TeacherConfig) Service {
 	return &teacherService{
 		teacherRepo: repo,
+		options:     *opts,
+		weekNumber:  opts.StartingWeek,
 	}
+}
+
+func (s *teacherService) Start() error {
+	slog.Info("Starting", "service", logger.ServiceTeacher)
+	c := cron.New(cron.WithLocation(time.Local))
+	_, err := c.AddFunc("0 0 * 0", func() {
+		slog.Info("Updating week number", logger.ServiceTeacher)
+		if s.weekNumber == 1 {
+			s.weekNumber = 2
+			return
+		}
+		s.weekNumber = 1
+	})
+	if err != nil {
+		slog.Info("Cron error", "error", err, "service", logger.ServiceTeacher)
+	}
+	c.Start()
+	s.cronScheduler = c
+	slog.Info("Started", "service", logger.ServiceTeacher)
+	return nil
+}
+
+func (s *teacherService) Stop(ctx context.Context) {
+	<-ctx.Done()
+	s.cronScheduler.Stop()
+	slog.Info("Stopped", "service", logger.ServiceTeacher)
 }
 
 func (s *teacherService) FindTeachersForTime(ctx context.Context, targetTime time.Time, auditorium int) []model.Teacher {
--- internal/teacher/storage.go
+++ internal/teacher/storage.go
@@ -2,11 +2,11 @@ package teacher
 
 import (
 	"context"
-	"database/sql"
 	"time"
 
 	"github.com/Ademun/mining-lab-bot/pkg/errs"
 	"github.com/Ademun/mining-lab-bot/pkg/model"
+	"github.com/jmoiron/sqlx"
 )
 
 type Repo interface {
@@ -14,51 +14,19 @@ type Repo interface {
 }
 
 type teacherRepo struct {
-	db *sql.DB
+	db *sqlx.DB
 }
 
-func NewRepo(ctx context.Context, db *sql.DB) (Repo, error) {
-	query := `
-create table if not exists teachers (
-    id integer primary key,
-    name text not null,
-    auditorium integer not null,
-    week_number integer not null,
-    weekday integer not null,
-    time_start text not null,
-    time_end text not null
-)
-`
-	_, err := db.ExecContext(ctx, query)
-	if err != nil {
-		return nil, &errs.ErrQueryExecution{Operation: "Init", Query: query, Err: err}
-	}
-
-	return &teacherRepo{db: db}, nil
+func NewRepo(db *sqlx.DB) Repo {
+	return &teacherRepo{db: db}
 }
 
 func (t *teacherRepo) FindByWeekNumberWeekdayAuditorium(ctx context.Context, weekNumber int, weekday time.Weekday, auditorium int) ([]model.Teacher, error) {
 	query := `select id, name, auditorium, week_number, weekday, time_start, time_end from teachers where week_number = ? and weekday = ? and auditorium = ?`
-
-	rows, err := t.db.QueryContext(ctx, query, weekNumber, weekday, auditorium)
-	if err != nil {
-		return nil, &errs.ErrQueryExecution{Operation: "FindByWeekNumberWeekday", Query: query, Err: err}
-	}
-	defer rows.Close()
-
 	var teachers []model.Teacher
-	for rows.Next() {
-		var teacher model.Teacher
-		err := rows.Scan(&teacher.ID, &teacher.Name, &teacher.Auditorium, &teacher.WeekNumber, &teacher.Weekday, &teacher.TimeStart, &teacher.TimeEnd)
-		if err != nil {
-			return nil, &errs.ErrRowIteration{Operation: "FindByWeekNumberWeekday", Query: query, Err: err}
-		}
-		teachers = append(teachers, teacher)
+	err := t.db.SelectContext(ctx, &teachers, query, weekNumber, weekday, auditorium)
+	if err != nil {
+		return nil, &errs.ErrQueryExecution{Operation: "FindByWeekNumberWeekdayAuditorium", Query: query, Err: err}
 	}
-
-	if err := rows.Err(); err != nil {
-		return nil, &errs.ErrRowIteration{Operation: "FindByWeekNumberWeekday", Query: query, Err: err}
-	}
-
 	return teachers, nil
 }
